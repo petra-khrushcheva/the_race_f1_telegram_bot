b52cd67ad7c65d2118cadd1047997ead18bd4535
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-20 19:28:37 +0500

    initial commit/Users/yuliyakhrushcheva/Dev/the_race_f1_telegram_bot/venv/bin/python

--- src/scraper/services.py
+++ src/scraper/services.py
@@ -1,3 +1,6 @@
+import requests
+
+from bs4 import BeautifulSoup
 from sqlalchemy import Result, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -5,9 +8,16 @@ from scraper.models import Article
 from core.database import get_session
 
 
-async def get_latest_articles(soup):
-    pass
+URL = "https://www.the-race.com/formula-1/"
 
+
+async def get_latest_articles():
+    response = requests.get(url=URL)
+    soup = BeautifulSoup(response.text, "lxml")
+    slugs = soup.find_all("a", class_="archive_width")
+    print(slugs)
+
+get_latest_articles()
 
 # вытаскивает из супа список слагов и разворачивает его, возвращает список
 
@@ -15,21 +25,25 @@ async def get_latest_articles(soup):
 async def get_articles_from_db():
     session: AsyncSession = get_session()
     result: Result = await session.execute(
-        select(Article.slug).order_by(Article.created_at) #блин, здесь desc or asc???
+        select(Article.slug).order_by(Article.created_at)
     )
     return result.scalars().all()
 
 
 async def save_article_to_db(slug):
-    pass
+    session: AsyncSession = get_session()
+    new_article = Article(slug=slug)
+    session.add(new_article)
+    await session.commit()
 
 
-# создает строку в бд
-
-
-async def refresh_db_articles(new_slug):
-    pass
-#  удалить старейшую строку из бд и сохранить новую
+async def refresh_db_articles(slug):
+    session: AsyncSession = get_session()
+    oldest_article = session.execute(
+        select(Article).order_by(Article.created_at).limit(1)
+    )
+    await session.delete(oldest_article)
+    await save_article_to_db(slug=slug)
 
 
 async def initial_scraping():
