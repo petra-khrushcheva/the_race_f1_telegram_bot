61bed6f6be846cdb0ec72545c7bc35a947e7b960
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-20 14:44:11 +0500

    add basemodels

---
+++ src/core/basemodels.py
@@ -0,0 +1,13 @@
+import datetime
+
+from sqlalchemy import DateTime, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=text("TIMEZONE('utc', now())")
+    )
