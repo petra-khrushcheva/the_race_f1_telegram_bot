295b40db5ee3bc4387f6a4093d3ad520c3190854
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-22 15:02:29 +0500

    fix async_session

--- src/core/database.py
+++ src/core/database.py
@@ -5,8 +5,3 @@ from core.config import settings
 engine = create_async_engine(url=settings.database_url, echo=settings.db_echo)
 
 async_session = async_sessionmaker(bind=engine)
-
-
-async def get_session():
-    async with async_session() as session:
-        yield session
